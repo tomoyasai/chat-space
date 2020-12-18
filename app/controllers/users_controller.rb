aabd6ed5e50ce3071017cb6f128cf5217d233f7a
tomoyasai <sugi97421yama84619@gmail.com>
2020-12-18 20:38:54 +0900

    非同期通信の結果をdone関数で受取り、ビューに追加するためのHTMLを作成
    Ajaxの通信の処理が正しく行われ、値がdoneで引数として受け取れる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def index
-    binding.pry
     return nil if params[:keyword] == ""
     @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
     respond_to do |format|
