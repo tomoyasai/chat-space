ba9065f912c77daec186a532c0bfa573b10e2730
tomoyasai <sugi97421yama84619@gmail.com>
2020-12-18 20:21:07 +0900

    イベント時に非同期通信で入力したデータを送信しましょう
    サーバーでは入力した文字を含むユーザーを検索し、検索結果をjson形式で返す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def index
+    binding.pry
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
     respond_to do |format|
       format.html
       format.json
