1011ac7e58427320626ccd7f6b4ce9365139ec90
tomoyasai <sugi97421yama84619@gmail.com>
2020-12-20 13:06:02 +0900

    機能のブラッシュアップ
    メッセージを取得したら画面がスクロールする/自動更新が必要ない画面では行わなようにする

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,9 +1,8 @@
 $(function(){
   function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
     if (message.image) {
       let html = //メッセージに画像が含まれる場合のHTMLを作る
-        `<div class="MessageBox" data-message-id=${message.id}>
+        `<div class="message__box" data-message-id=${message.id}>
           <div class="message__box__part">
             <div class="message__box__part__name">
               ${message.user_name}
@@ -22,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html = //メッセージに画像が含まれない場合のHTMLを作る
-      `<div class="MessageBox" data-message-id=${message.id}>
+      `<div class="message__box" data-message-id=${message.id}>
         <div class="message__box__part">
           <div class="message__box__part__name">
             ${message.user_name}
@@ -58,41 +57,11 @@ $(function(){
       $('.message-list').append(html);      
       $('form')[0].reset();
       $('.message-list').animate({ scrollTop: $('.message-list')[0].scrollHeight});
-      $('input').prop('disabled', false);
+      $('.submit-btn').prop('disabled', false);
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.submit-btn').prop('disabled', false);
     });
   });
-
-  let reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
-      if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.MessageField').append(insertHTML);
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
-  setInterval(reloadMessages, 7000);
 });
